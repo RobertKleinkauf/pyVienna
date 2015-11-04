0bffd665c6a006fb5a29c0e069763f710ea1095e
RobertKleinkauf <robkleinkauf@gmail.com>
2015-11-04 12:02:01 +0100

    Remove functions 'accessibility'

--- vienna.py
+++ vienna.py
@@ -35,47 +35,17 @@ def getBPPM(sequence, structure = "", bppm_cutoff = 0.00001):
     return bppm
     
     
-def getAccuracy(struct_stack, bppm):
+def getAccuracy(struct_dict, bppm):
     """
         Calculate average structuredness of given structure(stack) within bppm
+        struct 
+
     """
     acc = 0
-    for sq_i in struct_stack.keys():
-        v = str(sq_i) + "_" + str(struct_stack[sq_i])
+    for sq_i in struct_dict.keys():
+        v = str(sq_i) + "_" + str(struct_dict[sq_i])
         if v in bppm:
             acc += bppm[v]
             #acc += math.pow(bppm[v], 2) / len(struct_stack)
     return acc
 
-
-def getAccessibility(positions, l, bppm):
-    """
-        Calculate average unstructuredness of given positions within bppm of a sequence with length l
-    """
-    acc = 0
-    for sq_i in positions.keys():
-        #visits = 0
-        #vis = []
-        for i in xrange(1, sq_i ):
-            #visits += 1
-            #vis.append(i)
-            v = str(i) + "_" + str(sq_i)
-            if v in bppm:
-                if bppm[v] > 0:
-                    
-                    #val = math.pow(bppm[v], 2)
-                    val = bppm[v]
-                    acc += val
-        for i in xrange(sq_i + 1, l + 1):
-            #visits += 1
-            #vis.append(i)
-            v = str(sq_i) + "_" + str(i)
-            if v in bppm:
-                if bppm[v] > 0:
-                    #val = math.pow(bppm[v], 2)
-                    val = bppm[v]
-                    acc += val
-        #print "Visits", visits
-        #print vis
-   # print "used positions", len(positions)
-    return 1 -(acc/len(positions))
